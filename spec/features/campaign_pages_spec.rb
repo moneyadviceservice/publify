d1fdd43c538b9db0ce8f0204cdce81458f26fece
David Kewal <david.kewal@gmail.com>
2014-12-22 11:29:27 +0000

    Refactor the campaign feature spec
    Create a couple of campaign factories to test actual loading of
    campaigns

--- spec/features/campaign_pages_spec.rb
+++ spec/features/campaign_pages_spec.rb
@@ -1,5 +1,7 @@
 feature 'Campaigns' do
-  let(:campaigns_page) { CampaignsPage.new }
+  let(:campaigns_page)        { CampaignsPage.new }
+  let(:fake_campaign)         { create(:campaign, title: 'Save money at the supermarket') }
+  let(:second_fake_campaign)  { create(:campaign, title: 'Only fools and horses') }
 
   background do
     create(:blog)
@@ -7,12 +9,18 @@ feature 'Campaigns' do
     campaigns_page.load
   end
 
-  scenario 'Accessing the campaigns page' do
-    i_view_the_campaigns_page
+  scenario 'Viewing all campaigns' do
+    when_i_view_the_campaigns_page_as_an_admin
+    i_can_see_all_campaigns
   end
 
-  def i_view_the_campaigns_page
+  def when_i_view_the_campaigns_page_as_an_admin
     expect(campaigns_page).to be_displayed
+  end
+
+  def i_can_see_all_campaigns
+    expect(page).to have_content(fake_campaign.title)
+    expect(page).to have_content(second_fake_campaign.title)
   end
 
 end
