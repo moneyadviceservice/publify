0ebcaa54f4d6e9ce8c4f58fda2f61bf959a53e4f
Jon Alport <jon.alport@moneyadviceservice.org.uk>
2015-06-03 16:09:00 +0100

    Update compiled assets

---
+++ public/assets/ckeditor/config-cf1253a6aed289e07d2b394307412695.js
@@ -0,0 +1,33 @@
+CKEDITOR.editorConfig = function (config) {
+  config.filebrowserImageBrowseLinkUrl = '/admin/ckeditor/pictures';
+  config.filebrowserImageBrowseUrl = '/admin/ckeditor/pictures';
+  config.filebrowserImageUploadUrl = '/admin/ckeditor/pictures';
+  config.filebrowserBrowseUrl = 'admin/ckeditor/attachment_files';
+  config.filebrowserUploadUrl = 'admin/ckeditor/attachment_files';
+  config.extraPlugins = 'sourcedialog';
+  config.allowedContent = true;
+  config.contentsCss = '/assets/ckeditor/editor.css';
+  config.toolbar =
+  [
+    {
+      name: 'styles',
+      items: ['Format']
+    },
+    {
+      name: 'basicstyles',
+      items: ['Bold', 'Italic', 'Underline', '-', 'RemoveFormat']
+    },
+    {
+      name: 'paragraph',
+      items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-']
+    },
+    {
+      name: 'links',
+      items: ['Link', 'Unlink']
+    },
+    {
+      name: 'insert',
+      items: ['Image', 'Sourcedialog']
+    }
+  ];
+};
