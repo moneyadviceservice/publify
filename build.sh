873e073ef5fe55d1ecef7bfb46af38ee309f4d97
David Kewal <david.kewal@gmail.com>
2015-07-15 11:11:47 +0100

    Create build script for Go

---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -l
+
+set -e
+
+unset RUBYOPT
+export PATH=./bin:$PATH
+export BUNDLE_WITHOUT="test:development"
+
+version_number=${GO_PIPELINE_COUNTER-0}
+revision=`git rev-parse HEAD`
+build_date=`date +'%Y-%m-%d %H:%M %z'`
+
+cat > public/version <<EOT
+{
+  "version":"$version_number",
+  "buildDate":"$build_date",
+  "gitRevision":"$revision"
+}
+EOT
+
+echo "Cleaning temporary files"
+echo "----"
+rm -rf public/assets vendor/cache coverage log/* tmp/*
+
+echo "Running Bundle package"
+echo "----"
+bundle package --all
+
+echo "Running Bower cache clean"
+echo "----"
+bower cache clean
+
+echo "Purging bower components"
+echo "----"
+[ -d vendor/assets/bower_components ] && rm -r vendor/assets/bower_components
+
+echo "Running Bower update (via bowndler)"
+echo "----"
+bowndler update --production --config.interactive=false
+
+echo "Precompiling assets"
+echo "----"
+RAILS_ENV=production RAILS_GROUPS=assets rake assets:precompile
+
+echo "Uploading assets"
+echo "----"
+/usr/local/bin/upload-blog-assets.sh $(pwd)/public
+
+echo "Creating RPM"
+echo "----"
+cd ..
+/usr/local/rpm_builder/create-rails-rpm $artifact_name $artifact_name $version
+
+#prune packaged gems
+rm -rf vendor/cache
