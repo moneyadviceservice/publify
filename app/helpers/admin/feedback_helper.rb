5fc28cd57d5e8eb848f6819f2d3a187eaa50e702
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2016-01-20 16:10:04 +0000

    Refactor feedback admin. Got rid of url_for.

--- app/helpers/admin/feedback_helper.rb
+++ app/helpers/admin/feedback_helper.rb
@@ -18,11 +18,11 @@ module Admin::FeedbackHelper
   end
 
   def button_to_edit_comment(item)
-    link_to(content_tag(:span, '', class: 'glyphicon glyphicon-pencil'), { controller: 'admin/feedback', action: 'edit', id: item.id }, class: 'btn btn-primary btn-xs btn-action')
+    link_to(content_tag(:span, '', class: 'glyphicon glyphicon-pencil'), edit_admin_feedback_path(item), class: 'btn btn-primary btn-xs btn-action')
   end
 
   def button_to_delete_comment(item)
-    link_to(content_tag(:span, '', class: 'glyphicon glyphicon-trash'), { controller: 'admin/feedback', action: 'destroy', id: item.id }, class: 'btn btn-danger btn-xs btn-action')
+    link_to(content_tag(:span, '', class: 'glyphicon glyphicon-trash'), remove_admin_feedback_path(item), class: 'btn btn-danger btn-xs btn-action')
   end
 
   def button_to_actual_comment(item)
@@ -34,7 +34,7 @@ module Admin::FeedbackHelper
     klass = (item.state.to_s.downcase =~ /spam/) ? 'up' : 'down'
     klass2 = (item.state.to_s.downcase =~ /spam/) ? 'success' : 'warning'
 
-    link_to(content_tag(:span, '', class: "glyphicon glyphicon-thumbs-#{klass}"), { controller: 'admin/feedback', action: 'change_state', id: item.id, context: context }, class: "btn btn-#{klass2} btn-xs btn-action", remote: true)
+    link_to(content_tag(:span, '', class: "glyphicon glyphicon-thumbs-#{klass}"), change_state_admin_feedback_path(item, context: context), class: "btn btn-#{klass2} btn-xs btn-action", remote: true)
   end
 
 end
