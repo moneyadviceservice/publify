a9392161a1a58ace1c072cd25a1c61ee72213f08
Tomas D'Stefano <tomas_stefano@successoft.com>
2016-05-10 15:18:54 +0100

    Extracting build & test common to pre-build.sh

--- test.sh
+++ test.sh
@@ -6,16 +6,10 @@ export PATH=./bin:$PATH
 export RAILS_ENV=test
 export BUNDLE_WITHOUT=development
 
-CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
-BUNDLE_JOBS=$((CORES-1))
+echo 'Running Database Schema Load'
+echo '-------------------'
+RAILS_ENV=test bundle exec rake db:schema:load
 
-CI_PIPELINE_COUNTER=${GO_PIPELINE_COUNTER-0}
-CI_EXECUTOR_NUMBER=${EXECUTOR_NUMBER-0}
-
-bundle install --jobs $BUNDLE_JOBS
-bower cache clean
-rm -rf vendor/assets/bower_components
-bowndler update --production --config.interactive=false
-bundle exec rake db:schema:load
-
+echo 'Running RSpec tests'
+echo '-------------------'
 bundle exec rspec spec --format html --out tmp/spec.html --format RspecJunitFormatter --profile --format progress --deprecation-out log/rspec_deprecations.txt
