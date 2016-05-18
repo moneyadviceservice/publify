99ef2527e30a6b2cbaea2408e7c056f868f0f95a
pmcostello <pmcostello@hotmail.co.uk>
2016-05-18 12:00:39 +0100

    change asset variable to Azure storage containers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV['BLOG_ASSET_HOST_URL']
+  config.action_controller.asset_host = ENV['AZURE_ASSETS_STORAGE_BLOG_URL']
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
