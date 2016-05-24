0f5117929e392c28bc2734ee97f773c9c1790c2a
Misha Gorodnitzky <misha.gorodnitzky@moneyadviceservice.org.uk>
2016-05-24 16:03:25 +0100

    Remove account from azure asset container var

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ else
     config.storage = :azure
     config.azure_storage_account_name = ENV['AZURE_ASSETS_STORAGE_BLOG_ACCOUNT_NAME']
     config.azure_storage_access_key = ENV['AZURE_ASSETS_STORAGE_BLOG_ACCOUNT_KEY']
-    config.azure_container = ENV['AZURE_ASSETS_STORAGE_BLOG_ACCOUNT_CONTAINER']
+    config.azure_container = ENV['AZURE_ASSETS_STORAGE_BLOG_CONTAINER']
     config.asset_host = ENV['AZURE_ASSETS_STORAGE_BLOG_URL']
   end
 end
