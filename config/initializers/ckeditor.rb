a4166c26e54ea85e7ca1002237885d8689752874
Jiannis Georgiadis <jiannis.georgiadis@moneyadviceservice.org.uk>
2016-05-27 12:36:05 +0100

    :monkey: patch ckeditor to return a relative or absolute URL

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -12,3 +12,39 @@ Ckeditor.setup do |config|
     redirect_to main_app.accounts_path unless authorized?
   end
 end
+
+require 'ckeditor/asset_response'
+
+module Ckeditor
+  class AssetResponse
+    def success(relative_url_root = nil)
+      if json?
+        {
+            json: { "uploaded" => 1, "fileName" => asset.filename, "url" => asset.url }.to_json
+        }
+      elsif ckeditor?
+        {
+            text: %Q"<script type='text/javascript'>
+            window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, '#{asset_url(relative_url_root)}');
+          </script>"
+        }
+      else
+        {
+            json: asset.to_json(only: [:id, :type])
+        }
+      end
+    end
+
+    private
+
+    def asset_url(relative_url_root)
+      url = Ckeditor::Utils.escape_single_quotes(asset.url_content)
+
+      if URI(url).relative?
+        "#{relative_url_root}#{url}"
+      else
+        url
+      end
+    end
+  end
+end
