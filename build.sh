ff9b05bf1934124a5dc3c00e7771801126532b46
Misha Gorodnitzky <misha.gorodnitzky@moneyadviceservice.org.uk>
2016-07-11 11:13:07 +0100

    Ensure public/cache has the right ownership in RPM.
    This has been hard to do with the external 'create-rails-rpm' script we
    have, so we've added RPM generation to the build.sh script here.

--- build.sh
+++ build.sh
@@ -9,6 +9,26 @@ version_number=${GO_PIPELINE_LABEL-0}
 revision=`git rev-parse HEAD`
 build_date=`date +'%Y-%m-%d %H:%M %z'`
 
+function create_rails_rpm {
+  service_name="mas-$1"
+  shift
+  version_number=$1
+  shift
+  options="$@"
+  source_dir=$(pwd)
+
+  (
+    cd ..
+    rm -Rf build
+    mkdir -p build/srv build/etc/init.d
+    cp $source_dir/etc/after-install build/
+    cp $source_dir/etc/service-script build/etc/init.d/$service_name
+    rsync -va --exclude='.git*' --include=bin/rails --exclude=bin/* --exclude=features/ --exclude=log/ --exclude=spec/ --exclude=tmp/ $source_dir build/srv
+    cd build
+    fpm --after-install after-install --version $version_number --name $service_name --prefix=/ -a all -t rpm -s dir $options srv etc
+  )
+}
+
 cat > public/version <<EOT
 {
   "version":"$version_number",
@@ -31,5 +51,4 @@ echo '----'
 
 echo 'Creating RPM'
 echo '----'
-cd ..
-/usr/local/rpm_builder/create-rails-rpm $artifact_name $artifact_name $version_number
+create_rails_rpm $artifact_name $version_number --rpm-attr 0755,mas,service:/srv/blog/public/cache 
