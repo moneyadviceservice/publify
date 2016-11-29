50d413be3ee3c5d7a93b1db8d0e758415e926fde
Stephen Haley <stephen.haley@cultivatehq.com>
2016-11-29 15:53:34 +0000

    Add SupportsAmp option when creating article

--- app/controllers/amp_articles_controller.rb
+++ app/controllers/amp_articles_controller.rb
@@ -1,7 +1,11 @@
 class AmpArticlesController < ActionController::Base
   def show
     if (@article = Article.find_by_permalink(params[:from])).present?
-      render 'articles/amp/show'
+      if @article.supports_amp? || params[:no_redirect]
+        render 'articles/amp/show'
+      else
+        redirect_to article_url(@article.permalink)
+      end
     else
       render 'errors/404', status: 404
     end
