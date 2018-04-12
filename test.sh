753205376b52253c712269503ddb69f186fc6be3
pmcostello <pmcostello@hotmail.co.uk>
2018-04-12 13:29:40 +0100

    update test script

--- test.sh
+++ test.sh
@@ -26,7 +26,8 @@ bowndler update --production --config.interactive=false
 
 echo 'Running Database Schema Load'
 echo '-------------------'
-RAILS_ENV=test bundle exec rake db:schema:load
+cp config/database.yml.postgres config/database.yml
+RAILS_ENV=test bundle exec rake db:drop db:create db:schema:load db:migrate
 
 echo 'Running RSpec tests'
 echo '-------------------'
