442ffbddc4c9cd0fea7b261f874f1e649653ab30
Giuseppe Lobraico <Giuseppe.Lobraico@MoneyAdviceService.org.uk>
2018-11-16 14:42:27 +0000

    Adjust feature specs to work with the app mounted in the subpath

--- spec/support/pages/existing_campaigns_page.rb
+++ spec/support/pages/existing_campaigns_page.rb
@@ -1,8 +1,8 @@
 class ExistingCampaignsPage < SitePrism::Page
-  set_url "/admin/campaigns/{id}/edit"
-  set_url_matcher /campaigns/
+  set_url '/blog/admin/campaigns/{id}/edit'
+  set_url_matcher(/campaigns/)
 
-  element :campaign_title, "#campaign_title"
+  element :campaign_title, '#campaign_title'
 
   element :submit, "input[value='Save']"
 
--- spec/support/pages/new_campaigns_page.rb
+++ spec/support/pages/new_campaigns_page.rb
@@ -1,21 +1,21 @@
 class NewCampaignsPage < SitePrism::Page
-  set_url "/admin/campaigns/new"
-  set_url_matcher /campaigns/
+  set_url '/blog/admin/campaigns/new'
+  set_url_matcher(/campaigns/)
 
-  element :campaign_title, "#campaign_title"
-  element :description, "#campaign_description"
-  element :active, "#campaign_active"
+  element :campaign_title, '#campaign_title'
+  element :description, '#campaign_description'
+  element :active, '#campaign_active'
 
-  element :hero_image, "#campaign_hero_image"
-  element :full_bleed, "#campaign_full_bleed"
+  element :hero_image, '#campaign_hero_image'
+  element :full_bleed, '#campaign_full_bleed'
 
-  element :primary_link_type, "#campaign_primary_link_attributes_link_type"
-  element :primary_link_title, "#campaign_primary_link_attributes_title"
-  element :primary_link_url, "#campaign_primary_link_attributes_url"
+  element :primary_link_type, '#campaign_primary_link_attributes_link_type'
+  element :primary_link_title, '#campaign_primary_link_attributes_title'
+  element :primary_link_url, '#campaign_primary_link_attributes_url'
 
-  element :secondary_link_type, "#campaign_secondary_link_attributes_link_type"
-  element :secondary_link_title, "#campaign_secondary_link_attributes_title"
-  element :secondary_link_url, "#campaign_secondary_link_attributes_url"
+  element :secondary_link_type, '#campaign_secondary_link_attributes_link_type'
+  element :secondary_link_title, '#campaign_secondary_link_attributes_title'
+  element :secondary_link_url, '#campaign_secondary_link_attributes_url'
 
   element :submit, "input[value='Save']"
 
